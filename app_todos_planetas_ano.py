ba90c0f633ee08351cd3f959caa8aa15d1c8d5dc
Marcelo Fronza Welter <marcelo_fw@outlook.com>
2026-01-10 17:26:57 -0300

    updating

--- app_todos_planetas_ano.py
+++ app_todos_planetas_ano.py
@@ -92,7 +92,7 @@ if st.sidebar.button("Calcular Revolução", use_container_width=True):
         fig = make_subplots(
             rows=len(alvos_input), cols=1,
             subplot_titles=[f"<b>{a['planeta']} Natal em {a['signo']} {a['grau']}°</b>" for a in alvos_input],
-            vertical_spacing=0.025,
+            vertical_spacing=0.04,
             shared_xaxes=True
         )
 
@@ -177,7 +177,7 @@ if st.sidebar.button("Calcular Revolução", use_container_width=True):
         fig.update_layout(
             height=520 * len(alvos_input), # Altura proporcional ao número de alvos
             title=dict(text=f"<b>Revolução Planetária {ano_analise}</b>", x=0.5, y=0.98, yanchor="top", font = dict(size = 24)),
-            template='plotly_white',
+            template='plotly_dark',
             hovermode='x unified', dragmode='pan', margin=dict(t=240, b=50, l=50, r=50),
             legend=dict(orientation="h", yanchor="top", y=0.97, yref="container", xanchor="center", x=0.5)
         )
