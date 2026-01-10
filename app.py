6751e78084207df8a2e3759d64abe3e224786b5e
Marcelo Fronza Welter <marcelo_fw@outlook.com>
2026-01-10 16:22:07 -0300

    updating

--- app.py
+++ app.py
@@ -289,7 +289,7 @@ def secao_previsao_ia(ano, planeta_selecionado, signo_selecionado, grau_input, l
             with col_central:
                 if ativos_ia:
                     data_hora_str = f"{data_consulta.strftime('%d/%m/%Y')} às {hora_valida}"
-                    prompt_final = f"""Data e hora: {data_hora_str}.\n Trânsitos ativos para {planeta_selecionado} a {grau_input}° de {signo_selecionado}: \n{'; \n'.join(ativos_ia)}."""
+                    prompt_final = f"""Data e hora: {data_hora_str}.\nTrânsitos ativos para {planeta_selecionado} a {grau_input}° de {signo_selecionado}: \n{'; \n'.join(ativos_ia)}."""
                     st.write("### 📝 Sua consulta está pronta!")
                     st.text_area("Resultado dos trânsitos:", value=prompt_final, height=200)
                 else:
