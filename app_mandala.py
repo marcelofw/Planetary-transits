42f3c74a6362a7da71844b904a26b23a7a149d59
Marcelo Fronza Welter <marcelo_fw@outlook.com>
2026-01-13 21:16:39 -0300

    updating

--- app_mandala.py
+++ app_mandala.py
@@ -74,8 +74,8 @@ SIGNOS = ["Áries", "Touro", "Gêmeos", "Câncer", "Leão", "Virgem",
           "Libra", "Escorpião", "Sagitário", "Capricórnio", "Aquário", "Peixes"]
 
 SIMBOLOS_SIGNOS_UNICODE = [
-    "♈", "♉", "♊", "♋", "♌", "♍", 
-    "♎", "♏", "♐", "♑", "♒", "♓"
+    "♈\ufe0e", "♉\ufe0e", "♊\ufe0e", "♋\ufe0e", "♌\ufe0e", "♍\ufe0e", 
+    "♎\ufe0e", "♏\ufe0e", "♐\ufe0e", "♑\ufe0e", "♒\ufe0e", "♓\ufe0e"
 ]
 
 CORES_SIGNOS = {
