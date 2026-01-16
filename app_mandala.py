c3aca98f54be62ad3ff46e4b5f3922e01fe0ed53
Marcelo Fronza Welter <marcelo_fw@outlook.com>
2026-01-16 14:29:51 -0300

    updating

--- app_mandala.py
+++ app_mandala.py
@@ -365,43 +365,6 @@ def criar_mandala_astrologica(dt):
             textfont=dict(size=44, color=p["cor"], family="'DejaVu Sans', 'Segoe UI Symbol', 'Apple Symbols', sans-serif"), 
             showlegend=False, hovertemplate=hover_template
         ))
-    
-    # Ascendente
-    if asc_valor is not None:
-        asc_graus = int(asc_valor % 30)
-        asc_minutos = int((asc_valor - int(asc_valor)) * 60)
-        indice_signo_asc = int(asc_valor / 30)
-        nome_signo_asc = SIGNOS[indice_signo_asc]
-        simbolo_signo_asc = SIMBOLOS_SIGNOS_UNICODE[indice_signo_asc]
-        cor_elemento_asc = CORES_SIGNOS.get(simbolo_signo_asc, "black")
-        hover_template_asc = f"Ascendente<br>{nome_signo_asc}<br>{asc_graus}°{asc_minutos}'<extra></extra>"
-        
-        # # Anotações ASC
-        # fig.add_trace(go.Scatterpolar(r=[7.4], theta=[asc_valor], mode='text', text='Asc', 
-        #                             textfont=dict(size=26, color='black', family="Trebuchet MS"), 
-        #                             showlegend=False, hovertemplate=hover_template_asc))
-        # Marcadores internos
-        fig.add_trace(go.Scatterpolar(r=[raio_interno], theta=[asc_valor], mode='markers', 
-                                    marker=dict(size=8, color='black', line=dict(color='black', width=0)), 
-                                    showlegend=False, hovertemplate=hover_template_asc))
-        # Marcadores externos
-        fig.add_trace(go.Scatterpolar(r=[8.0], theta=[asc_valor], mode='markers', 
-                                    marker=dict(size=8, color='black', line=dict(color='black', width=0)), 
-                                    showlegend=False, hovertemplate=hover_template_asc))
-        # Anotações Graus
-        fig.add_trace(go.Scatterpolar(r=[6.3], theta=[asc_valor], mode='text', text=[f"{asc_graus:02d}°"], 
-                                    textfont=dict(size=25, color="black", family="Trebuchet MS"), 
-                                    showlegend=False, hovertemplate=hover_template_asc))
-        # Anotações Minutos
-        fig.add_trace(go.Scatterpolar(r=[4.1], theta=[asc_valor], mode='text', text=[f"{asc_minutos:02d}'"], 
-                                    textfont=dict(size=21, color="black", family="Trebuchet MS"), 
-                                    showlegend=False, hovertemplate=hover_template_asc))
-        # Anotações Símbolo dos Planetas
-        fig.add_trace(go.Scatterpolar(r=[5.2], theta=[asc_valor], mode='text', text=[simbolo_signo_asc], 
-                                    textfont=dict(size=32, color=cor_elemento_asc, family="DejaVu Sans"), 
-                                    showlegend=False, hovertemplate=hover_template))
-
-
 
     # --- LAYOUT FINAL ---
     fig.update_layout(
